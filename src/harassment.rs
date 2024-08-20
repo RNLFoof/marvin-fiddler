1860a3c8d2227069d3710833eaa9c934a2996a02
RNLFoof <z.zablotsky@gmail.com>
2024-08-20 17:59:01 -0400

    chore: put up some more scaffolding

--- src/harassment.rs
+++ src/harassment.rs
@@ -1,3 +1,5 @@
+use crate::marvin::Task;
+
 fn contemplate_harassment() {
     todo!()
 }
@@ -6,12 +8,40 @@ fn harass() {
     todo!()
 }
 
-type Cultivar = Box<dyn Fn() -> Vec<String>>;
-
-fn harvest(cultivars: Vec<Cultivar>) -> String {
+fn harvest(cultivars: Vec<cultivars::Cultivar>) -> String {
     todo!()
 }
 
 fn vocalize(this: String) {
     todo!()
+}
+
+struct Context {
+    tracked_task: Task,
+    todays_tasks: Vec<Task>,
+}
+
+mod cultivars {
+    use std::time::Duration;
+
+    pub struct Cultivar {
+        harvest_interval: Duration,
+        harvest_behavior: Box<dyn Fn() -> Vec<String>>,
+    }
+
+    fn current_time() -> String {
+        todo!()
+    }
+
+    fn fraction_of_duration() -> String {
+        todo!()
+    }
+
+    fn duration_exceeded() -> String {
+        todo!()
+    }
+
+    fn prioritize() -> String {
+        todo!()
+    }
 }
